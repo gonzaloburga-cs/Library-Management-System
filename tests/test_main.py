c6a553e90a9d044c9aa6be3e2cd1a018e384a400
Caden Atack <cadenthomas4@gmail.com>
2025-09-26 13:33:08 -0600

    Gahlee this is tricky stuff. Added some more test cases and am commiting so that Gonzalo can compare to documentation

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,16 @@ import os
 import pytest
 from fastapi.testclient import TestClient
 from backend.server import app
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, mock_open
 import backend.main as cli
 
 
 client = TestClient(app)
 
+
+
+
+#interaction with server
 def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
@@ -23,7 +27,9 @@ def test_get_books_server():
     assert "data" in response.json()
 
 
-#print_menu
+
+#Isolated tests for CLI functions (not interacting with server)
+
 
 def test_print_menu(capsys):
     if "token" in globals():
@@ -43,13 +49,54 @@ def test_print_menu(capsys):
     assert "6. Exit" in output
 
 
-#hello_world
 
 #login
+@patch("builtins.input", side_effect=["rootbeerlover@yahoo.com"]) #mock input for email
+@patch("backend.main.maskpass.askpass", return_value="Barqs0nT0p!")
+@patch("backend.main.requests.get")
+@patch("backend.main.requests.post")
+def test_login_success(mock_post, mock_get, mock_pass, mock_input, tmp_path):
+    post_response = MagicMock()
+    post_response.status_code = 200
+    post_response.text = "fake_token"
+    mock_post.return_value = post_response
+
+    get_response = MagicMock()
+    get_response.status_code = 200
+    mock_get.return_value = get_response
+
+
+    with patch("builtins.open", mock_open()) as mock_file:
+        cli.login()
+
+    mock_file.assert_called_with("token.txt", "r")
+
+
+@patch("builtins.input", side_effect=["bugjelly@uvu.edu", "n"]) #mock input for email; n for retry
+@patch("backend.main.maskpass.askpass", return_value="bugjam")
+@patch("backend.main.requests.post")
+def test_login_failure(mock_post, mock_pass, mock_input, capsys, tmp_path):
+    token_file = tmp_path / "token.txt"
+    cli.token_file_path = str(token_file)
+    if token_file.exists():
+        token_file.unlink()
+
+    post_response = MagicMock()
+    post_response.status_code = 401        #triggers !=200 block in login()
+    post_response.reason = "Unauthorized"
+    post_response.text = ""
+    mock_post.return_value = post_response
+
+    cli.login()
+
+    captured = capsys.readouterr()
+    output = captured.out
+    assert "Login failed. Status code 401 for reason Unauthorized." in output
+    assert "Please check your credentials and try again." in output
 
 #get_books
 @patch('backend.main.requests.get')
-def test_get_books(mock_get):
+def test_get_books(mock_get, capsys):
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = {
@@ -59,7 +106,7 @@ def test_get_books(mock_get):
                 "created_at": "2025-09-26T11:00:00",
                 "updated_at": "2025-09-26T11:00:00",
                 "title": "Book 1",
-                "author": "Author 1",
+                "author": "Frank Herbert",
                 "isbn": "111-111",
                 "is_checked_out": False
             },
@@ -75,16 +122,82 @@ def test_get_books(mock_get):
         ]
     }
     mock_get.return_value = mock_response
-
     books = cli.get_books()
+    captured = capsys.readouterr()
+    output = captured.out
+    assert "Book 1" in output
+    assert "Book 2" in output
+    assert "2" in output
+    assert "1" in output
+    assert "Author" in output
+    assert "ISBN" in output
+    assert "ID" in output
+    assert "111-111" in output
+    assert "Frank Herbert" in output
+
+
 
 
 #add_book
 
+#patch decorators are applied bottom to top
+@patch("builtins.input", side_effect=["Atlas Shrugged", "Ayn Rand", "123-456"]) #input for the 3 input calls
+@patch("backend.main.requests.put") #requests.put is now the first parameter in test (mock_put)
+def test_add_book(mock_put, mock_input, capsys):
+    cli.token = "fake_token" #simulate being logged in
+    mock_put.return_value.status_code = 200
+    cli.add_book()
+    captured = capsys.readouterr()
+    output = captured.out
+    assert "Book added successfully!" in output
+    mock_put.assert_called_once()
+
+    del cli.token  # reset token for later tests
+
+
+@patch("builtins.input", side_effect=["Atlas Shrugged", "Ayn Rand", "123-456"]) #input for the 3 input calls
+@patch("backend.main.requests.put")
+def test_add_book_500(mock_put, mock_input, capsys):
+    cli.token = "fake_token"
+    mock_put.return_value.status_code = 500
+    cli.add_book()
+    captured = capsys.readouterr()
+    output = captured.out
+    assert "Failed to add book. Status code: " in output
+    mock_put.assert_called_once()
+    del cli.token
+
+
+
+
+
+
+
 #checkout_book
+
+@patch("builtins.input", side_effect=["1"]) #input for the 1 input call
+@patch("backend.main.get_books")
+def test_checkout_book(mock_get_books, mock_input, capsys):
+    cli.token = "fake_token"
+    cli.checkout_book()
+    captured = capsys.readouterr()
+    output = captured.out
+    assert "Book with ID 1 checked out successfully!" in output
+    mock_get_books.assert_called_once()
+    del cli.token
+
 
 #return_book
 
 #clear_screen
+
+@patch("backend.main.os.system")
+def test_clear_screen(mock_os):
+    cli.clear_screen()
+    if sys.platform == "win32":
+        mock_os.assert_called_once_with("cls")
+    else:
+        mock_os.assert_called_once_with("clear")
+
 
 #main?

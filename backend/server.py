39d7377cd5b61f790d6a0d2f458d55bd38a5f619
NickJar <Nickjar86@gmail.com>
2025-09-25 18:30:55 -0600

    Add endpoints for checkout, return, signup, and user (#9)
    Co-authored-by: Nick Jardine <njardine@freshlime.com>

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+
 from fastapi import FastAPI, Depends, Request
 
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from datetime import datetime, timezone
 
 from dotenv import load_dotenv
 
@@ -46,6 +49,26 @@ def get_books():
     response = (supabase.table("books").select("*").execute())
     return response
 
+@app.post("/signup")
+async def signup(request: Request):
+    data = await request.json()
+
+    create_user = supabase.auth.sign_up({"email": data["email"], "password": data["password"]})
+
+    supabase_id = create_user.user.id
+    supabase_service_client.table("users").insert({"supabase_id": supabase_id}).execute()
+
+    return "User created successfully"
+
+
+@app.get("/user")
+async def get_user(request: Request):
+    auth_token = request.headers["Authorization"]
+    user = supabase.auth.get_user(auth_token)
+    response = user.user.id
+
+    return response
+
 
 # Returns a temporary auth token of the user whose credentials were provided
 @app.post("/auth")
@@ -74,4 +97,46 @@ async def create_book(request: Request):
 
     return response
 
+
+@app.put("/checkout")
+async def create_book(request: Request):
+    data = await request.json()
+    auth_token = request.headers["Authorization"]
+    supabase.postgrest.auth(auth_token)
+
+    # check if the book is already checked out
+    is_checked_out = supabase.table("books").select("*").match({"id": data["book_id"], "is_checked_out": True}).execute()
+    if is_checked_out.data:
+        response = "This Book is not currently available for checkout"
+    else:
+        supabase.table("checkout_logs").upsert({"book_id": data["book_id"], "user_id": data["user_id"]}).execute()
+        supabase.table("books").update({"is_checked_out": True}).eq("id", data["book_id"]).execute()
+        response = "Book successfully checked out"
+
+    return response
+
+
+@app.put("/return")
+async def create_book(request: Request):
+    data = await request.json()
+    auth_token = request.headers["Authorization"]
+    current_date = datetime.now(timezone.utc)
+    supabase.postgrest.auth(auth_token)
+
+    # queries
+    is_checked_out = "SELECT * FROM checkout_logs WHERE checkin_date IS NULL AND book_id = :book_id"
+    return_book = "UPDATE checkout_logs SET checkin_date = :current_date where checkin_date IS NULL AND book_id = :book_id"
+
+    with engine.connect() as connection:
+        result = connection.execute(text(is_checked_out), {"book_id": data["book_id"]})
+        if result.rowcount == 0:
+            response = "This book isn't currently checked out by you"
+        else:
+            supabase.table("books").update({"is_checked_out": False}).eq("id", data["book_id"]).execute()
+            connection.execute(text(return_book), {"book_id": data["book_id"],
+                                                   "current_date": current_date.strftime('%Y-%m-%d %H:%M:%S %z')})
+            connection.commit()
+            response = "Book successfully returned"
+
+    return response
 
